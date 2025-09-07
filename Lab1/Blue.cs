1a47444e5dcf565e122a05cfdbbb26966a1f10cc
dysthmia <maksim.fediarin@mail.ru>
2025-09-07 23:47:26 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -9,6 +9,14 @@ namespace Lab1
             // тестик
 
             // code here
+            if ((a > 0 && b > 0) || (a < 0 && b < 0) || (a == 0 && b == 0))
+            {
+                answer= true;
+            }
+            else
+            {
+                answer= false;
+            }
 
             // end
 
@@ -19,6 +27,14 @@ namespace Lab1
             bool answer = false;
 
             // code here
+            if (Math.Abs(d)-Math.Abs((int)d) > 0)
+            {
+                answer= true;
+            }
+            else
+            {
+                answer= false;
+            }
 
             // end
 
@@ -29,7 +45,18 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            if (b == 0)
+            {
+                answer= false;
+            }
+            else if (a % b == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer= false;
+            }
             // end
 
             return answer;
@@ -39,7 +66,25 @@ namespace Lab1
             double answer = 0;
 
             // code here
-
+            var dd = Math.Abs(d);
+            var ff = Math.Abs(f);
+            var gg = Math.Abs(g);
+            double[] array = new double[3];
+            array[0] = dd;
+            array[1] = ff;
+            array[2] = gg;
+            if (array.Max() == dd)
+            {
+                answer= d;
+            }
+            else if (array.Max() == ff)
+            {
+                answer=f;
+            }
+            else if (array.Max() == gg)
+            {
+                answer= g;
+            }
             // end
 
             return answer;
@@ -49,6 +94,20 @@ namespace Lab1
             double answer = 0;
 
             // code here
+            if (x <= -1)
+            {
+                answer= 0;
+            }
+
+            if (x > -1 && x <= 0)
+            {
+                answer= x+ 1;
+            }
+
+            if (x > 0)
+            {
+                answer= 1;
+            }
 
             // end
 
@@ -59,7 +118,16 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            var SideSq=Math.Sqrt(squareS);
+            var DiamCircle = 2*(Math.Sqrt(circleS/3.14));
+            if (SideSq >=DiamCircle)
+            {
+                answer= true;
+            }
+            else 
+            {
+                answer= false;
+            }
             // end
 
             return answer;
@@ -70,7 +138,28 @@ namespace Lab1
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer= -1;
+                }
+                else
+                {
+                    answer= 0;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer= 1;
+                }
+                else
+                {
+                    answer= 0;
+                }
+            }
             // end
 
             return answer;
@@ -80,7 +169,25 @@ namespace Lab1
             bool answer = false;
 
             // code here
+            int NowA = a / 2;
+            int NowB = b / 2;
+            int NowC = c / 2;
             
+            int NowSum = NowA + NowB + NowC;
+
+            if (NowSum == 0)
+            {
+                answer = false;
+            }
+
+            else if (NowSum % 3 == 0 || (NowSum+1) % 3 == 0)
+            {
+                answer= true;
+            }
+            else
+            {
+                answer= false;
+            }
             // end
 
             return answer;
